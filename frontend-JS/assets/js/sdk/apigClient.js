e67948cdb23a3f49d66b0c2225444c43c8e3207e
PrateekKumar1709 <85689959+PrateekKumar1709@users.noreply.github.com>
2021-10-09 18:31:09 -0400

    Removed credentials for safety reasons

--- frontend-JS/assets/js/sdk/apigClient.js
+++ frontend-JS/assets/js/sdk/apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://eojlcjnznh.execute-api.us-east-1.amazonaws.com/Dev';
+    var invokeUrl = '';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
