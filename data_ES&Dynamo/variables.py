1a423f3c588e99a0bf78979ae3d30166bd1bd904
PrateekKumar1709 <85689959+PrateekKumar1709@users.noreply.github.com>
2021-10-09 18:28:34 -0400

    Removed Key

--- data_ES&Dynamo/variables.py
+++ data_ES&Dynamo/variables.py
@@ -7,13 +7,13 @@ from datetime import datetime
 from elasticsearch import Elasticsearch
 from botocore.exceptions import ClientError
 
-API_KEY = 'g90aWZAKlnXpydx_-9kbX5b0TvvThxKi0kR0LvF4vYAiItIlSjNswiywHhFcKlTusp-Iz-6GTBCHInwzSJyLx9SCWbvaznobu9moPUMTn-hdovAWKiVavkn8RV1TYXYx'
+API_KEY = ''
 ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
 HEADERS = {'Authorization': 'bearer %s' % API_KEY}
 
-ES_USER = 'prateek'
-ES_PASS = 'Prateek@1709'
-ES_URL = 'https://search-restaurants-tjdy4o57veqr6t73mf4rgl4o44.us-east-1.es.amazonaws.com/restaurants/Restaurant/{count}'
+ES_USER = ''
+ES_PASS = ''
+ES_URL = ''
 
 DYNAMO_DB_TABLE_NAME = 'yelp-restaurants'
 
